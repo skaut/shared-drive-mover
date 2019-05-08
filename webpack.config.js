659286f0a7df064e8c155a6531183cb67dd79489
GenaBitu <genabitu@gmail.com>
2019-05-09 00:29:29 +0200

    Added frontend index

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 
 module.exports = {
   mode: 'development',
@@ -8,11 +10,17 @@ module.exports = {
       {from: 'src/appsscript.json', to: '.'},
       {from: 'src/backend', to: '.'},
       {from: 'src/frontend', to: '.'}
-    ])
+    ]),
+    new HtmlWebpackPlugin({
+      inlineSource: '.(js)$'
+    }),
+    new HtmlWebpackInlineSourcePlugin()
   ],
-  entry: './src/main.js',
+  entry: {
+    frontend: './src/frontend/index.js'
+  },
   output: {
-    filename: 'main.bundle.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist')
   }
 };
