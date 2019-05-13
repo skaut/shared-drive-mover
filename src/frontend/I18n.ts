d14f1a2ca3caaae280896d3f4ae600460f855cdf
GenaBitu <genabitu@gmail.com>
2019-05-13 14:40:26 +0200

    Added Czech translation

--- src/frontend/I18n.ts
+++ src/frontend/I18n.ts
@@ -18,8 +18,8 @@ export default new VueI18n({
 			},
 			dialogs: {
 				nonEmpty: {
-					title: 'Not empty',
-					content: 'The selected Shared drive is not empty. Do you want to proceed anyway?',
+					title: 'Shared drive not empty',
+					content: 'The selected destination Shared drive is not empty. Do you want to proceed anyway?',
 					confirm: 'Yes',
 					cancel: 'No'
 				},
@@ -35,7 +35,7 @@ export default new VueI18n({
 				description: 'When moving files of which you are not the owner, instead of moving the file a copy is created in the destination Shared drive. For such a case, there are several options that can be configured:',
 				copy: {
 					title: 'Copy comments',
-					description: 'Copy comments from the original files. The comments will be created as if you made them, with the original commenter tagged. Note that may slow the moving significantly.'
+					description: 'Copy comments from the original files. The comments will be created as if you made them, with the original commenter tagged. Note that this may slow down the moving.'
 				}
 			},
 			confirmation: {
@@ -46,6 +46,47 @@ export default new VueI18n({
 			},
 			folderSelector: {
 				myDrive: 'My Drive'
+			}
+		},
+		cs: {
+			steps: {
+				folderSelection: 'Výběr zdrojové složky',
+				sharedDriveSelection: 'Výběr cílového Sdíleného disku',
+				configuration: 'Nastavení',
+				confirmation: 'Potvrzení',
+				progress: 'Přesun',
+				done: 'Hotovo'
+			},
+			dialogs: {
+				nonEmpty: {
+					title: 'Sdílený disk není prázdný',
+					content: 'Vybraný cílový Sdílený disk není prázdný. Chcete přesto pokračovat?',
+					confirm: 'Ano',
+					cancel: 'Ne'
+				},
+				error: {
+					title: 'Chyba',
+					content: 'Nastala neznámá chyba. Prosím zkontrolujte zdrojovou složku a cílový Sdílený disk a zkuste to znovu.',
+					optionalErrorMessage: 'Chybová hláška: '
+				}
+			},
+			continueButton: 'Pokračovat',
+			moveButton: 'Přesunout',
+			configuration: {
+				description: 'U souborů, jichž nejste vlastníkem, se místo jejich přesunutí vytvoří kopie v cílovém Sdíleném disku. V takovém případě lze nastavit několik možností:',
+				copy: {
+					title: 'Kopírovat komentáře',
+					description: 'Zkopírovat komentáře z původních souborů. Komentáře budou vytvořeny pod vaším účtem a původní autor bude označen. Kopírování komentářů může přesun zpomalit.'
+				}
+			},
+			confirmation: {
+				description: 'Opravdu chcete přesunout obsah složky "{folderName}" do Sdíleného disku "{sharedDriveName}"?'
+			},
+			done: {
+				description: 'Úspěšně přesunuto. Můžete smazat původní soubory ze svého Google Disku - některé soubory mohly být zanechány protože nemohly být smazány automaticky.'
+			},
+			folderSelector: {
+				myDrive: 'Můj Disk'
 			}
 		}
 	}
