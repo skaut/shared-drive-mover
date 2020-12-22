c3d6f68dbb779783642e44249f0a896de013592d
Marek Dědič <developer@dedic.eu>
2020-12-22 20:10:13 +0100

    Added logic for Error reporting in frontend

--- src/frontend/Done.vue
+++ src/frontend/Done.vue
@@ -1,13 +1,37 @@
 <template>
   <div>
-    {{ $t("done.description") }}
+    <p>
+      {{ $t("done.description") }}
+    </p>
+    <div v-if="errors.length > 0">
+      <p>
+        {{ $t("done.errorHeader") }}
+      </p>
+      <md-table>
+        <md-table-row>
+          <md-table-head> {{ $t("done.file") }} </md-table-head>
+          <md-table-head> {{ $t("done.message") }} </md-table-head>
+        </md-table-row>
+        <md-table-row v-for="error in errors" :key="error.file">
+          <md-table-cell> {{ error.file.join("/") }} </md-table-cell>
+          <md-table-cell> {{ error.error }} </md-table-cell>
+        </md-table-row>
+      </md-table>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 
-export default Vue.extend({});
+export default Vue.extend({
+  props: {
+    errors: {
+      type: Array,
+      default: (): Array<string> => [],
+    },
+  },
+});
 </script>
 
 <style scoped>
