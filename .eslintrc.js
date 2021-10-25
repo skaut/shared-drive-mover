33499b39decab6adc26fc7d20bdf825f4aff1fc8
Marek Dědič <developer@dedic.eu>
2021-10-25 22:41:23 +0200

    Fixed linting on eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,25 @@
+/* eslint-env node */
+
 module.exports = {
   parserOptions: {
     parser: "@typescript-eslint/parser",
     project: "./tsconfig.json",
-    sourceType: "script"
+    sourceType: "script",
   },
   env: {
     browser: true,
-    node: false
+    node: false,
   },
-  plugins: [
-    "svelte3",
-    "@typescript-eslint"
-  ],
+  plugins: ["svelte3", "@typescript-eslint"],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "plugin:prettier/recommended"
+    "plugin:prettier/recommended",
   ],
   rules: {
     "no-warning-comments": "warn",
-    "@typescript-eslint/array-type": ["error", {default: "generic"}],
+    "@typescript-eslint/array-type": ["error", { default: "generic" }],
     "@typescript-eslint/consistent-indexed-object-style": "error",
     "@typescript-eslint/consistent-type-assertions": "error",
     "@typescript-eslint/consistent-type-definitions": "error",
@@ -37,7 +36,7 @@ module.exports = {
     "@typescript-eslint/no-extraneous-class": "error",
     "@typescript-eslint/no-invalid-void-type": "error",
     "@typescript-eslint/no-meaningless-void-operator": "error",
-    "@typescript-eslint/no-namespace": ["error", {allowDeclarations: true}],
+    "@typescript-eslint/no-namespace": ["error", { allowDeclarations: true }],
     "@typescript-eslint/no-non-null-asserted-nullish-coalescing": "error",
     "@typescript-eslint/no-parameter-properties": "error",
     "@typescript-eslint/no-unnecessary-boolean-literal-compare": "error",
@@ -61,31 +60,62 @@ module.exports = {
     "@typescript-eslint/strict-boolean-expressions": "error",
     "@typescript-eslint/switch-exhaustiveness-check": "error",
     "@typescript-eslint/unified-signatures": "error",
-    "@typescript-eslint/no-non-null-assertion": "off"
+    "@typescript-eslint/no-non-null-assertion": "off",
   },
   settings: {
-    "svelte3/typescript": require("typescript")
+    "svelte3/typescript": require("typescript"),
   },
   overrides: [
     {
       files: ["*.svelte"],
-      processor: "svelte3/svelte3"
+      processor: "svelte3/svelte3",
     },
     {
-      files: ["gulpfile.js", "frontend.webpack.config.js"],
+      parserOptions: {
+        project: null,
+      },
+      files: ["gulpfile.js", "frontend.webpack.config.js", ".eslintrc.js"],
       rules: {
+        "@typescript-eslint/await-thenable": "off",
+        "@typescript-eslint/no-base-to-string": "off",
+        "@typescript-eslint/no-confusing-void-expression": "off",
+        "@typescript-eslint/no-floating-promises": "off",
+        "@typescript-eslint/no-implied-eval": "off",
+        "@typescript-eslint/no-meaningless-void-operator": "off",
+        "@typescript-eslint/no-misused-promises": "off",
+        "@typescript-eslint/no-unnecessary-boolean-literal-compare": "off",
+        "@typescript-eslint/no-unnecessary-condition": "off",
+        "@typescript-eslint/no-unnecessary-qualifier": "off",
+        "@typescript-eslint/no-unnecessary-type-assertion": "off",
+        "@typescript-eslint/no-unsafe-argument": "off",
         "@typescript-eslint/no-unsafe-assignment": "off",
         "@typescript-eslint/no-unsafe-call": "off",
         "@typescript-eslint/no-unsafe-member-access": "off",
         "@typescript-eslint/no-unsafe-return": "off",
-        "@typescript-eslint/no-var-requires": "off"
-      }
-    }
+        "@typescript-eslint/no-var-requires": "off",
+        "@typescript-eslint/non-nullable-type-assertion-style": "off",
+        "@typescript-eslint/prefer-includes": "off",
+        "@typescript-eslint/prefer-nullish-coalescing": "off",
+        "@typescript-eslint/prefer-readonly": "off",
+        "@typescript-eslint/prefer-reduce-type-parameter": "off",
+        "@typescript-eslint/prefer-regexp-exec": "off",
+        "@typescript-eslint/prefer-return-this-type": "off",
+        "@typescript-eslint/prefer-string-starts-ends-with": "off",
+        "@typescript-eslint/promise-function-async": "off",
+        "@typescript-eslint/require-array-sort-compare": "off",
+        "@typescript-eslint/require-await": "off",
+        "@typescript-eslint/restrict-plus-operands": "off",
+        "@typescript-eslint/restrict-template-expressions": "off",
+        "@typescript-eslint/strict-boolean-expressions": "off",
+        "@typescript-eslint/switch-exhaustiveness-check": "off",
+        "@typescript-eslint/unbound-method": "off",
+      },
+    },
   ],
   globals: {
     google: "readonly",
     MoveError: "readonly",
     MoveResponse: "readonly",
-    NamedRecord: "readonly"
-  }
-}
+    NamedRecord: "readonly",
+  },
+};
--- src/frontend/index.ts
+++ src/frontend/index.ts
@@ -1,5 +1,6 @@
 import App from "./App.svelte";
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
 new App({
   target: document.body,
 });
