136cb491a1890f1f72b1f7d2aa89dffa121d43b6
Marek Dědič <developer@dedic.eu>
2021-11-30 17:54:12 +0100

    Code splitting

--- src/backend/move.ts
+++ src/backend/move.ts
@@ -1,17 +1,7 @@
+import { isFolderEmpty } from "./move/isFolderEmpty";
 import { moveFolderContents } from "./move/moveFolderContents";
 
 import type { MoveResponse } from "../interfaces/MoveResponse";
-
-function isFolderEmpty(folderID: string): boolean {
-  const response = Drive.Files!.list({
-    q: '"' + folderID + '" in parents and trashed = false',
-    includeItemsFromAllDrives: true,
-    supportsAllDrives: true,
-    maxResults: 1,
-    fields: "items(id)",
-  });
-  return response.items!.length === 0;
-}
 
 export function move(
   sourceID: string,
