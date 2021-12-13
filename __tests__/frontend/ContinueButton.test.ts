cd6da06a987ddbea5a2b6711664465b7a3993ab5
Marek Dědič <developer@dedic.eu>
2021-12-13 13:21:41 +0100

    Added tests for ContinueButton next event

--- __tests__/frontend/ContinueButton.test.ts
+++ __tests__/frontend/ContinueButton.test.ts
@@ -1,6 +1,7 @@
 import "@testing-library/jest-dom";
 import { mocked } from "ts-jest/utils";
 import { render } from "@testing-library/svelte";
+import userEvent from "@testing-library/user-event";
 
 import ContinueButton from "../../src/frontend/ContinueButton.svelte";
 
@@ -8,7 +9,7 @@ import * as svelteI18n from "svelte-i18n";
 
 jest.mock("svelte-i18n");
 
-test("TEST", () => {
+test("ContinueButton works", () => {
   type FormatXMLElementFn<T, R = Array<T | string> | T | string> = (
     parts: Array<T | string>
   ) => R;
@@ -40,8 +41,14 @@ test("TEST", () => {
       return (): void => {}; // eslint-disable-line @typescript-eslint/no-empty-function
     }
   );
-  const { getByText } = render(ContinueButton, {
+
+  const { component, getByText } = render(ContinueButton, {
     disabled: false,
   });
+  const onNext = jest.fn();
+  component.$on("next", onNext);
   expect(getByText("continue.buttonLabel")).toBeInTheDocument();
+  expect(onNext.mock.calls.length).toBe(0);
+  userEvent.click(getByText("continue.buttonLabel"));
+  expect(onNext.mock.calls.length).toBe(1);
 });
