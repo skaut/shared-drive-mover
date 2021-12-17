1622dac259c360c3f9e7e9e79e2fc0911dc814dd
Marek Dědič <developer@dedic.eu>
2021-12-17 19:03:03 +0100

    More comprehensive tests for Confirmation

--- __tests__/frontend/Confirmation.test.ts
+++ __tests__/frontend/Confirmation.test.ts
@@ -1,6 +1,6 @@
 import "@testing-library/jest-dom";
 import { render } from "@testing-library/svelte";
-//import userEvent from "@testing-library/user-event";
+import userEvent from "@testing-library/user-event";
 
 import Confirmation from "../../src/frontend/Confirmation.svelte";
 import { mockSvelteI18n } from "../test-utils/mock-svelte-i18n";
@@ -10,17 +10,39 @@ jest.mock("svelte-i18n");
 test("Confirmation works", () => {
   const svelteI18nRunFn = mockSvelteI18n();
   const { component, getByText } = render(Confirmation, {
-    source: null,
-    destination: null,
+    sourcePath: [
+      { id: "SRC_PATH_ID_1", name: "SRC_PATH_NAME_1" },
+      { id: "SRC_PATH_ID_2", name: "SRC_PATH_NAME_2" },
+      { id: "SRC_PATH_ID_3", name: "SRC_PATH_NAME_3" },
+    ],
+    destinationPath: [
+      { id: "DEST_PATH_ID_1", name: "DEST_PATH_NAME_1" },
+      { id: "DEST_PATH_ID_2", name: "DEST_PATH_NAME_2" },
+    ],
+    source: { id: "SRC_ID", name: "SRC_NAME" },
+    destination: { id: "DEST_ID", name: "DEST_NAME" },
   });
   const onPrevious = jest.fn();
   const onNext = jest.fn();
   component.$on("previous", onPrevious);
   component.$on("next", onNext);
   expect(getByText("steps.confirmation.introduction")).toBeInTheDocument();
+  expect(getByText("back.buttonLabel")).toBeInTheDocument();
   expect(getByText("steps.confirmation.buttonLabel")).toBeInTheDocument();
+  expect(svelteI18nRunFn.mock.calls).toHaveLength(4);
+  expect(svelteI18nRunFn.mock.calls[0]).toHaveLength(2);
+  expect(svelteI18nRunFn.mock.calls[0][1]!.values!.source).toBe(
+    "SRC_PATH_NAME_1/SRC_PATH_NAME_2/SRC_PATH_NAME_3/SRC_NAME"
+  );
+  expect(svelteI18nRunFn.mock.calls[0][1]!.values!.destination).toBe(
+    "DEST_PATH_NAME_1/DEST_PATH_NAME_2/DEST_NAME"
+  );
   expect(onPrevious.mock.calls).toHaveLength(0);
   expect(onNext.mock.calls).toHaveLength(0);
-  //userEvent.click(getByText("continue.buttonLabel"));
-  //expect(onNext.mock.calls).toHaveLength(1);
+  userEvent.click(getByText("back.buttonLabel"));
+  expect(onPrevious.mock.calls).toHaveLength(1);
+  expect(onNext.mock.calls).toHaveLength(0);
+  userEvent.click(getByText("steps.confirmation.buttonLabel"));
+  expect(onPrevious.mock.calls).toHaveLength(1);
+  expect(onNext.mock.calls).toHaveLength(1);
 });
