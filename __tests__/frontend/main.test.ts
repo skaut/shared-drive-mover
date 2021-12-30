3e997d8341753b2ca11e1b18618cd7741400712c
Marek Dědič <developer@dedic.eu>
2021-12-30 23:30:14 +0100

    Added the ability to respond with an error

--- __tests__/frontend/main.test.ts
+++ __tests__/frontend/main.test.ts
@@ -1,9 +1,11 @@
 Cypress.on("window:before:load", (win) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  type successHandlerType = (value?: any, object?: any) => void;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  type failureHandlerType = (error: Error, object?: any) => void;
   function endpoints(
-    successHandler: (
-      first?: google.script.Parameter | HTMLFormElement,
-      ...rest: Array<google.script.Parameter>
-    ) => void
+    successHandler: successHandlerType,
+    failureHandler: failureHandlerType
   ): object {
     return {
       listSharedDrives: cy.stub().callsFake(() => {
@@ -28,20 +30,24 @@ Cypress.on("window:before:load", (win) => {
         },
       },
       run: {
-        withFailureHandler: cy.stub().returns({
-          withSuccessHandler: cy
-            .stub()
-            .callsFake((successHandler: () => void) =>
-              endpoints(successHandler)
-            ),
-        }),
+        withFailureHandler: cy
+          .stub()
+          .callsFake((failureHandler: failureHandlerType) => ({
+            withSuccessHandler: cy
+              .stub()
+              .callsFake((successHandler: successHandlerType) =>
+                endpoints(successHandler, failureHandler)
+              ),
+          })),
         withSuccessHandler: cy
           .stub()
-          .callsFake((successHandler: () => void) => {
-            return {
-              withFailureHandler: cy.stub().returns(endpoints(successHandler)),
-            };
-          }),
+          .callsFake((successHandler: successHandlerType) => ({
+            withFailureHandler: cy
+              .stub()
+              .callsFake((failureHandler: failureHandlerType) =>
+                endpoints(successHandler, failureHandler)
+              ),
+          })),
         withUserObject: cy.stub(),
       },
       url: {
