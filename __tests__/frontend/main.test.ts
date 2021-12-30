6039d23054e4ab96c91279cee89cc687c5f65497
Marek Dědič <developer@dedic.eu>
2021-12-31 00:25:58 +0100

    Configurable endpoint stubs

--- __tests__/frontend/main.test.ts
+++ __tests__/frontend/main.test.ts
@@ -1,60 +1,80 @@
-Cypress.on("window:before:load", (win) => {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  type successHandlerType = (value?: any, object?: any) => void;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  type failureHandlerType = (error: Error, object?: any) => void;
-  function endpoints(
-    successHandler: successHandlerType,
-    failureHandler: failureHandlerType
-  ): object {
-    return {
-      listSharedDrives: cy.stub().callsFake(() => {
-        successHandler([]);
-      }),
-    };
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type SuccessHandlerType = (value?: any, object?: any) => void;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type FailureHandlerType = (error: Error, object?: any) => void;
+
+function stubEndpoints(
+  endpoints: Record<
+    string,
+    (
+      successHandler: SuccessHandlerType,
+      failureHandler: FailureHandlerType
+    ) => void
+  >
+): Record<string, sinon.SinonStub> {
+  const stubbedEndpoints: Record<string, sinon.SinonStub> = {};
+  function endpointFn(
+    successHandler: SuccessHandlerType,
+    failureHandler: FailureHandlerType
+  ): Record<string, sinon.SinonStub> {
+    for (const key in endpoints) {
+      stubbedEndpoints[key] = cy.stub().callsFake(() => {
+        endpoints[key](successHandler, failureHandler);
+      });
+    }
+    return stubbedEndpoints;
   }
-  win.google = {
-    script: {
-      history: {
-        push: cy.stub(),
-        replace: cy.stub(),
-        setChangeHandler: cy.stub(),
-      },
-      host: {
-        close: cy.stub(),
-        setHeight: cy.stub(),
-        setWidth: cy.stub(),
-        origin: "",
-        editor: {
-          focus: cy.stub(),
+  Cypress.on("window:before:load", (win) => {
+    win.google = {
+      script: {
+        history: {
+          push: cy.stub(),
+          replace: cy.stub(),
+          setChangeHandler: cy.stub(),
+        },
+        host: {
+          close: cy.stub(),
+          setHeight: cy.stub(),
+          setWidth: cy.stub(),
+          origin: "",
+          editor: {
+            focus: cy.stub(),
+          },
+        },
+        run: {
+          withFailureHandler: cy
+            .stub()
+            .callsFake((failureHandler: FailureHandlerType) => ({
+              withSuccessHandler: cy
+                .stub()
+                .callsFake((successHandler: SuccessHandlerType) =>
+                  endpointFn(successHandler, failureHandler)
+                ),
+            })),
+          withSuccessHandler: cy
+            .stub()
+            .callsFake((successHandler: SuccessHandlerType) => ({
+              withFailureHandler: cy
+                .stub()
+                .callsFake((failureHandler: FailureHandlerType) =>
+                  endpointFn(successHandler, failureHandler)
+                ),
+            })),
+          withUserObject: cy.stub(),
+        },
+        url: {
+          getLocation: cy.stub(),
         },
       },
-      run: {
-        withFailureHandler: cy
-          .stub()
-          .callsFake((failureHandler: failureHandlerType) => ({
-            withSuccessHandler: cy
-              .stub()
-              .callsFake((successHandler: successHandlerType) =>
-                endpoints(successHandler, failureHandler)
-              ),
-          })),
-        withSuccessHandler: cy
-          .stub()
-          .callsFake((successHandler: successHandlerType) => ({
-            withFailureHandler: cy
-              .stub()
-              .callsFake((failureHandler: failureHandlerType) =>
-                endpoints(successHandler, failureHandler)
-              ),
-          })),
-        withUserObject: cy.stub(),
-      },
-      url: {
-        getLocation: cy.stub(),
-      },
-    },
-  };
+    };
+  });
+  return stubbedEndpoints;
+}
+
+stubEndpoints({
+  listSharedDrives: (successHandler) => {
+    successHandler([]);
+  },
 });
 
 it("TEST-E2E", () => {
