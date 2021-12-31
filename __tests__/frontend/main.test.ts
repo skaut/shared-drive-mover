0d39468a73320ce659854546638012ef2ee4a9b5
Marek Dědič <developer@dedic.eu>
2021-12-31 12:19:15 +0100

    Complete walk through the app in FE tests

--- __tests__/frontend/main.test.ts
+++ __tests__/frontend/main.test.ts
@@ -72,8 +72,14 @@ function stubEndpoints(
 }
 
 stubEndpoints({
+  listFolders: (successHandler) => {
+    successHandler([]);
+  },
   listSharedDrives: (successHandler) => {
     successHandler([]);
+  },
+  move: (successHandler) => {
+    successHandler({ status: "success", errors: [] });
   },
 });
 
@@ -81,4 +87,11 @@ it("TEST-E2E", () => {
   cy.visit("http://localhost:8080");
   cy.contains("Shared drive mover");
   cy.contains("Continue").click();
+  cy.contains("My Drive").click();
+  cy.contains("Continue").click();
+  cy.contains("My Drive").click();
+  cy.contains("Continue").click();
+  cy.contains("Move").click();
+  cy.contains("Done!");
+  cy.contains("Successfully moved");
 });
