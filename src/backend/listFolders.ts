0ba27e2c26c928061bda57368a9363a8d7d5af65
Marek Dědič <developer@dedic.eu>
2022-01-07 10:17:19 +0100

    Catching API errors in backend listing

--- src/backend/listFolders.ts
+++ src/backend/listFolders.ts
@@ -4,41 +4,45 @@ import type { ListResponse } from "../interfaces/ListResponse";
 import type { NamedRecord } from "../interfaces/NamedRecord";
 
 export function listFolders(parentID: string): ListResponse {
-  const response = paginationHelper_<
-    GoogleAppsScript.Drive.Schema.FileList,
-    NamedRecord
-  >(
-    (pageToken) =>
-      Drive.Files!.list({
-        q:
-          '"' +
-          parentID +
-          '" in parents and (mimeType = "application/vnd.google-apps.folder" or (mimeType = "application/vnd.google-apps.shortcut" and shortcutDetails.targetMimeType = "application/vnd.google-apps.folder")) and trashed = false',
-        includeItemsFromAllDrives: true,
-        supportsAllDrives: true,
-        pageToken: pageToken,
-        maxResults: 1000,
-        fields:
-          "nextPageToken, items(id, title, mimeType, shortcutDetails(targetId))",
-      }),
-    (response) =>
-      response
-        .items!.sort((first, second) =>
-          first.title!.localeCompare(
-            second.title!,
-            Session.getActiveUserLocale()
+  try {
+    const response = paginationHelper_<
+      GoogleAppsScript.Drive.Schema.FileList,
+      NamedRecord
+    >(
+      (pageToken) =>
+        Drive.Files!.list({
+          q:
+            '"' +
+            parentID +
+            '" in parents and (mimeType = "application/vnd.google-apps.folder" or (mimeType = "application/vnd.google-apps.shortcut" and shortcutDetails.targetMimeType = "application/vnd.google-apps.folder")) and trashed = false',
+          includeItemsFromAllDrives: true,
+          supportsAllDrives: true,
+          pageToken: pageToken,
+          maxResults: 1000,
+          fields:
+            "nextPageToken, items(id, title, mimeType, shortcutDetails(targetId))",
+        }),
+      (response) =>
+        response
+          .items!.sort((first, second) =>
+            first.title!.localeCompare(
+              second.title!,
+              Session.getActiveUserLocale()
+            )
           )
-        )
-        .map((item) => {
-          const id =
-            item.mimeType === "application/vnd.google-apps.shortcut"
-              ? item.shortcutDetails!.targetId!
-              : item.id!;
-          return { id, name: item.title! };
-        })
-  );
-  return {
-    status: "success",
-    response,
-  };
+          .map((item) => {
+            const id =
+              item.mimeType === "application/vnd.google-apps.shortcut"
+                ? item.shortcutDetails!.targetId!
+                : item.id!;
+            return { id, name: item.title! };
+          })
+    );
+    return {
+      status: "success",
+      response,
+    };
+  } catch (e) {
+    return { status: "error", type: "DriveAPIError" };
+  }
 }
--- src/backend/listSharedDrives.ts
+++ src/backend/listSharedDrives.ts
@@ -4,22 +4,26 @@ import type { ListResponse } from "../interfaces/ListResponse";
 import type { NamedRecord } from "../interfaces/NamedRecord";
 
 export function listSharedDrives(): ListResponse {
-  const response = paginationHelper_<
-    GoogleAppsScript.Drive.Schema.DriveList,
-    NamedRecord
-  >(
-    (pageToken) =>
-      Drive.Drives!.list({
-        pageToken: pageToken,
-        maxResults: 100,
-        orderBy: "name",
-        fields: "nextPageToken, items(id, name)",
-      }),
-    (response) =>
-      response.items!.map((item) => ({ id: item.id!, name: item.name! }))
-  );
-  return {
-    status: "success",
-    response,
-  };
+  try {
+    const response = paginationHelper_<
+      GoogleAppsScript.Drive.Schema.DriveList,
+      NamedRecord
+    >(
+      (pageToken) =>
+        Drive.Drives!.list({
+          pageToken: pageToken,
+          maxResults: 100,
+          orderBy: "name",
+          fields: "nextPageToken, items(id, name)",
+        }),
+      (response) =>
+        response.items!.map((item) => ({ id: item.id!, name: item.name! }))
+    );
+    return {
+      status: "success",
+      response,
+    };
+  } catch (e) {
+    return { status: "error", type: "DriveAPIError" };
+  }
 }
