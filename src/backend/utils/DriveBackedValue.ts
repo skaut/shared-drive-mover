f30ae9df55f29b4447f1f24dc85e791096aadf0e
Marek Dědič <developer@dedic.eu>
2022-05-12 20:05:47 +0200

    Added ability to use existing Drive state file

--- src/backend/utils/DriveBackedValue.ts
+++ src/backend/utils/DriveBackedValue.ts
@@ -1,26 +1,36 @@
 export class DriveBackedValue_<T> {
   private static readonly DriveFolderName = "Shared drive mover cache";
 
-  private readonly key: string;
+  private readonly hash: string;
 
   public constructor(key: string) {
-    this.key = key;
+    this.hash = Utilities.computeDigest(
+      Utilities.DigestAlgorithm.SHA_256,
+      key,
+      Utilities.Charset.US_ASCII
+    )
+      .map((byte) => (byte < 0 ? byte + 256 : byte).toString(16).slice(-2))
+      .join("");
   }
 
   public saveValue(value: T): void {
-    let stateFolderId = this.getStateFolder();
-    if (stateFolderId === null) {
-      stateFolderId = this.createStateFolder();
-    }
-    this.saveStateFile(stateFolderId, value);
+    this.saveDriveFile(this.getDriveFolderId(), value);
   }
 
-  private getStateFolder(): string | null {
+  private getDriveFolderId(): string {
+    const stateFolderId = this.getExistingDriveFolderId();
+    if (stateFolderId !== null) {
+      return stateFolderId;
+    }
+    return this.createDriveFolder();
+  }
+
+  private getExistingDriveFolderId(): string | null {
     const response = Drive.Files!.list({
       q:
-        "title = '" +
+        'title = "' +
         DriveBackedValue_.DriveFolderName +
-        '\' and "root" in parents and mimeType = "application/vnd.google-apps.folder" and trashed = false',
+        '" and "root" in parents and mimeType = "application/vnd.google-apps.folder" and trashed = false',
       maxResults: 1,
       fields: "items(id)",
     });
@@ -33,7 +43,7 @@ export class DriveBackedValue_<T> {
     return null;
   }
 
-  private createStateFolder(): string {
+  private createDriveFolder(): string {
     const response = Drive.Files!.insert({
       mimeType: "application/vnd.google-apps.folder",
       title: DriveBackedValue_.DriveFolderName,
@@ -41,21 +51,55 @@ export class DriveBackedValue_<T> {
     return response.id!;
   }
 
-  private saveStateFile(stateFolderId: string, value: T): void {
-    const hash = Utilities.computeDigest(
-      Utilities.DigestAlgorithm.SHA_256,
-      this.key,
-      Utilities.Charset.US_ASCII
-    )
-      .map((byte) => (byte < 0 ? byte + 256 : byte).toString(16).slice(-2))
-      .join("");
+  private saveDriveFile(stateFolderId: string, value: T): void {
+    const fileId = this.getExistingDriveFileId(stateFolderId);
+    if (fileId !== null) {
+      this.updateExistingDriveFile(fileId, value);
+    } else {
+      this.saveAsNewDriveFile(stateFolderId, value);
+    }
+  }
+
+  private getExistingDriveFileId(stateFolderId: string): string | null {
+    const response = Drive.Files!.list({
+      q:
+        'title = "' +
+        this.getFileName() +
+        '" and "' +
+        stateFolderId +
+        '" in parents and trashed = false',
+      maxResults: 1,
+      fields: "items(id)",
+    });
+    if (
+      response.items!.length > 0 &&
+      typeof response.items![0].id === "string"
+    ) {
+      return response.items![0].id;
+    }
+    return null;
+  }
+
+  private updateExistingDriveFile(fileId: string, value: T): void {
+    Drive.Files!.update(
+      {},
+      fileId,
+      Utilities.newBlob(JSON.stringify(value), "application/json")
+    );
+  }
+
+  private saveAsNewDriveFile(stateFolderId: string, value: T): void {
     Drive.Files!.insert(
       {
         mimeType: "application/json",
         parents: [{ id: stateFolderId }],
-        title: "shared-drive-mover-state-" + hash + ".json",
+        title: this.getFileName(),
       },
       Utilities.newBlob(JSON.stringify(value), "application/json")
     );
+  }
+
+  private getFileName(): string {
+    return "shared-drive-mover-state-" + this.hash + ".json";
   }
 }
