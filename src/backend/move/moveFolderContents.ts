b5c5deb9b402cece495524da75c61f4e25b216aa
Marek Dědič <developer@dedic.eu>
2022-05-13 18:24:04 +0200

    Dead code removal

--- src/backend/move/moveFolderContents.ts
+++ src/backend/move/moveFolderContents.ts
@@ -22,47 +22,6 @@ function moveFolderContentsFiles_(
   }
 }
 
-/*
-function moveFolderContentsFolders_(
-  context: MoveContext_,
-  copyComments: boolean,
-  mergeFolders: boolean
-): void {
-  const subFolders = context.tryAndLog(() =>
-    listFoldersInFolder_(context.sourceID)
-  );
-  if (subFolders === null) {
-    return;
-  }
-  for (const folder of subFolders) {
-    context.tryAndLog(() => {
-      const destinationFolder = resolveDestinationFolder_(
-        folder,
-        context,
-        mergeFolders
-      );
-      moveFolderContents_(
-        context.childContext(folder.id!, destinationFolder.id!, folder.title!),
-        copyComments,
-        mergeFolders
-      );
-      deleteFolderIfEmpty_(folder.id!);
-    }, folder.title);
-  }
-}
-*/
-
-/*
-export function moveFolderContents_(
-  context: MoveContext_,
-  copyComments: boolean,
-  mergeFolders: boolean
-): void {
-  moveFolderContentsFiles_(context, copyComments);
-  moveFolderContentsFolders_(context, copyComments, mergeFolders);
-}
-*/
-
 export function moveFolder_(
   state: MoveState_,
   context: MoveContextv2_,
@@ -75,7 +34,7 @@ export function moveFolder_(
     listFoldersInFolder_(context.sourceID)
   );
   if (subFolders !== null) {
-    // TODO: Delete folders?
+    // TODO: Delete folders? (deleteFolderIfEmpty_)
     for (const folder of subFolders) {
       // TODO: Create destination folder later?
       const destinationFolder = resolveDestinationFolder_(
