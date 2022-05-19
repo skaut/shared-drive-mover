b9e781367c77434a0d1a0072e25e290521ea0b9d
Marek Dědič <developer@dedic.eu>
2022-05-19 10:58:57 +0200

    Repurposed MoveContext tests for MoveState

--- __tests__/backend/utils/MoveState.test.ts
+++ __tests__/backend/utils/MoveState.test.ts
@@ -1,46 +1,34 @@
 import { mocked } from "jest-mock";
 
-import { MoveContext_ } from "../../../src/backend/utils/MoveContext";
+import { mockedDriveBackedValue } from "../../test-utils/DriveBackedValue-stub";
+import { MoveState_ } from "../../../src/backend/utils/MoveState";
 
-import { ErrorLogger_ } from "../../../src/backend/utils/ErrorLogger";
+import { DriveBackedValue_ } from "../../../src/backend/utils/DriveBackedValue";
 
-jest.mock("../../../src/backend/utils/ErrorLogger");
+jest.mock("../../../src/backend/utils/DriveBackedValue", () => ({
+  DriveBackedValue_: jest.fn(),
+}));
 
-test("MoveContext constructs correctly", () => {
-  const logger = new ErrorLogger_();
-  const context = new MoveContext_(
-    "SRC_ID",
-    "DEST_ID",
-    ["PATH", "TO", "FILE"],
-    logger
+test("MoveState constructs correctly", () => {
+  const driveBackedValueMock = mockedDriveBackedValue();
+  mocked(DriveBackedValue_).mockReturnValue(driveBackedValueMock);
+
+  const state = new MoveState_("SRC_ID", "DEST_ID", false, false);
+
+  expect(mocked(DriveBackedValue_).mock.calls).toHaveLength(1);
+  expect(mocked(DriveBackedValue_).mock.calls[0][0]).toBe(
+    JSON.stringify({
+      sourceID: "SRC_ID",
+      destinationID: "DEST_ID",
+      copyComments: false,
+      mergeFolders: false,
+    })
   );
-
-  expect(context.sourceID).toBe("SRC_ID");
-  expect(context.destinationID).toBe("DEST_ID");
-  expect(context.path).toStrictEqual(["PATH", "TO", "FILE"]);
-  expect(context.logger).toBe(logger);
+  expect(state.getNextPath()).toBeNull();
+  expect(state.getErrors()).toStrictEqual([]);
 });
 
-test("MoveContext creates child context correctly", () => {
-  const logger = new ErrorLogger_();
-  const context = new MoveContext_(
-    "SRC_ID",
-    "DEST_ID",
-    ["PATH", "TO", "FILE"],
-    logger
-  );
-  const childContext = context.childContext(
-    "NEW_SRC_ID",
-    "NEW_DEST_ID",
-    "CHILD_NAME"
-  );
-
-  expect(childContext.sourceID).toBe("NEW_SRC_ID");
-  expect(childContext.destinationID).toBe("NEW_DEST_ID");
-  expect(childContext.path).toStrictEqual(["PATH", "TO", "FILE", "CHILD_NAME"]);
-  expect(childContext.logger).toBe(logger);
-});
-
+/*
 test("MoveContext.tryAndLog works correctly", () => {
   const logger = new ErrorLogger_();
   const context = new MoveContext_(
@@ -98,3 +86,4 @@ test("MoveContext.tryAndLog handles errors gracefully", () => {
   ]);
   expect(mocked(context.logger).log.mock.calls[0][1]).toBe("ERROR_MESSAGE");
 });
+*/
