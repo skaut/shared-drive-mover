87c39a4dfbe4273f836e79024fafb16eb8f0c5ff
Marek Dědič <developer@dedic.eu>
2022-08-28 14:44:35 +0200

    Added additional configuration test

--- __tests__/frontend/configuration.test.ts
+++ __tests__/frontend/configuration.test.ts
@@ -37,3 +37,27 @@ it("works with copy configuration", () => {
     );
   });
 });
+
+it("works with merge configuration", () => {
+  cy.visit("http://localhost:8080");
+  cy.contains("Shared drive mover");
+  cy.contains("Copy comments").click();
+  cy.contains("Continue").click();
+  cy.contains("My Drive").click();
+  cy.contains("Continue").click();
+  cy.contains("My Drive").click();
+  cy.contains("Continue").click();
+  cy.contains('contents of the folder "My Drive" into the folder "My Drive"');
+  cy.contains("Move").click();
+  cy.contains("is moving");
+  cy.contains("Done!");
+  cy.contains("Successfully moved").then(() => {
+    expect(stubs.move).to.have.been.calledOnceWith(
+      "root",
+      "root",
+      false,
+      true,
+      false
+    );
+  });
+});
