f257afdd2f1c6a072b9eaf578d1c3e2faec2635e
Marek Dědič <developer@dedic.eu>
2022-08-30 13:29:15 +0200

    Removed obsolete comment

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,7 +5,6 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   video: false,
   e2e: {
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
       require("@cypress/code-coverage/task")(on, config);
       return config;
