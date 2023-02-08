559e18fa24a311731fe92b82c923d587620da697
Marek Dědič <developer@dedic.eu>
2023-02-08 17:20:05 +0100

    Revert "Added @types/capress__code-coverage"
    This reverts commit a05281b0a97741dbf0ce84d10c14cfc91cbe4ae9.

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,12 @@
 /* eslint-env node */
 
-import register from "@cypress/code-coverage/task";
 import { defineConfig } from "cypress";
 
 export default defineConfig({
   video: false,
   e2e: {
     setupNodeEvents(on, config) {
-      register(on, config);
+      require("@cypress/code-coverage/task")(on, config);
       return config;
     },
     specPattern: "__tests__/frontend/**/*.cy.ts",
