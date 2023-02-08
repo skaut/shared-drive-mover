c46405dfcec128eba2d3da09ad89851f2621c7df
Marek Dědič <developer@dedic.eu>
2023-02-08 17:21:11 +0100

    Eslint disable for @cypress/code-coverage

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   video: false,
   e2e: {
     setupNodeEvents(on, config) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-var-requires
       require("@cypress/code-coverage/task")(on, config);
       return config;
     },
