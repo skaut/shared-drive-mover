0f2b56ca849c5e3c481886f106a4e23b764b82d7
Marek Dědič <developer@dedic.eu>
2023-02-08 17:30:52 +0100

    Fixed flaky test

--- __tests__/frontend/navigation.cy.ts
+++ __tests__/frontend/navigation.cy.ts
@@ -40,7 +40,6 @@ it("works with basic configuration", () => {
   cy.contains("Continue").click();
   cy.contains("Confirmation");
   cy.contains("Move").click();
-  cy.contains("is moving");
   cy.contains("Done!");
   cy.contains("Successfully moved").then(() => {
     expect(stubs.move).to.have.been.calledOnceWith(
