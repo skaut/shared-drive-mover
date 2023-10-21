cb08a1cf4425f68f40dfb1f2ed6ae4205f2807ab
Marek Dědič <developer@dedic.eu>
2023-10-22 00:31:23 +0200

    Ported destination selection API error test

---
+++ __tests__/frontend-playwright/destination-selection-api-error.spec.ts
@@ -0,0 +1,43 @@
+import { expect, test } from "@playwright/test";
+
+import { setup } from "../test-utils-playwright/stub-endpoints";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("/");
+  await setup(page);
+
+  await page.evaluate(() => {
+    window._endpointStubs.listFolders = {
+      status: "success",
+      value: { status: "error", type: "DriveAPIError" },
+    };
+    window._endpointStubs.listSharedDrives = {
+      status: "success",
+      value: {
+        status: "success",
+        response: [
+          { id: "ID_DRIVE_1", name: "DRIVE 1" },
+          { id: "ID_DRIVE_2", name: "DRIVE 2" },
+        ],
+      },
+    };
+  });
+});
+
+test("handles raw errors in source folder selection gracefully", async ({
+  page,
+}) => {
+  await expect(
+    page.getByText("Shared drive mover", { exact: true }),
+  ).toBeVisible();
+  await page.getByText("Continue").click();
+  await page.getByText("DRIVE 1").click();
+  await page.getByText("Continue").click();
+  await page.getByText("DRIVE 2").dblclick();
+  await expect(
+    page.getByText("An error occurred", { exact: true }),
+  ).toBeVisible();
+  await expect(
+    page.getByText("An error occurred in Google Drive"),
+  ).toBeVisible();
+});
