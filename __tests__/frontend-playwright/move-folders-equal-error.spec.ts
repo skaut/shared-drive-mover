ee042c60f1f7165973723729f3307b4f8378a92f
Marek Dědič <developer@dedic.eu>
2023-10-22 00:49:20 +0200

    Ported move source-destination folder equality error test

---
+++ __tests__/frontend-playwright/move-folders-equal-error.spec.ts
@@ -0,0 +1,47 @@
+import { expect, test } from "@playwright/test";
+
+import { setup } from "../test-utils-playwright/stub-endpoints";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("/");
+  await setup(page);
+
+  await page.evaluate(() => {
+    window._endpointStubs.listFolders = {
+      status: "success",
+      value: { status: "success", response: [] },
+    };
+    window._endpointStubs.listSharedDrives = {
+      status: "success",
+      value: { status: "success", response: [] },
+    };
+    window._endpointStubs.move = {
+      status: "success",
+      value: { status: "error", type: "sourceEqualsDestination" },
+    };
+  });
+});
+
+test("works with source and destination folders being equal", async ({
+  page,
+}) => {
+  await expect(
+    page.getByText("Shared drive mover", { exact: true }),
+  ).toBeVisible();
+  await page.getByText("Continue").click();
+  await page.getByText("My Drive").click();
+  await page.getByText("Continue").click();
+  await page.getByText("My Drive").click();
+  await page.getByText("Continue").click();
+  await expect(
+    page.getByText(
+      'contents of the folder "My Drive" into the folder "My Drive"',
+    ),
+  ).toBeVisible();
+  await page.getByText("Move", { exact: true }).click();
+  await expect(page.getByText("Confirmation")).toBeVisible();
+  await expect(page.getByText("An error occurred")).toBeVisible();
+  await expect(
+    page.getByText("The source and destination folders must be different"),
+  ).toBeVisible();
+});
