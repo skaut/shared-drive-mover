e44711e7796cbc5ebebb9e7eda89fea2b1d8babd
Marek Dědič <developer@dedic.eu>
2023-10-25 20:27:06 +0200

    Using arrow functions

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -8,25 +8,25 @@ import webpack from "webpack-stream";
 import backendWebpackConfig from "./backend.webpack.config.js";
 import frontendWebpackConfig from "./frontend.webpack.config.js";
 
-gulp.task("build:appsscript", function () {
-  return gulp.src("src/appsscript.json").pipe(gulp.dest("dist/"));
-});
+gulp.task("build:appsscript", () =>
+  gulp.src("src/appsscript.json").pipe(gulp.dest("dist/")),
+);
 
-gulp.task("build:frontend", function () {
-  return gulp
+gulp.task("build:frontend", () =>
+  gulp
     .src("src/frontend/index.ts")
     .pipe(webpack(frontendWebpackConfig(undefined, {})))
     .pipe(filter(["index.html"]))
     .pipe(replace("\u0085", "\\u0085"))
-    .pipe(gulp.dest("dist/"));
-});
+    .pipe(gulp.dest("dist/")),
+);
 
-gulp.task("build:backend", function () {
-  return gulp
+gulp.task("build:backend", () =>
+  gulp
     .src("src/backend/index.ts")
     .pipe(webpack(backendWebpackConfig))
-    .pipe(gulp.dest("dist/"));
-});
+    .pipe(gulp.dest("dist/")),
+);
 
 gulp.task(
   "build",
