a2668f5803a7491f6ac1eeaffd39f0cb83893746
Marek Dědič <developer@dedic.eu>
2024-01-21 20:06:10 +0100

    Moved PublicEndpoints declaration into backend index

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,7 +1,11 @@
+/// <reference types="google.script.client-side" />
+
 import { doGet } from "./doGet";
 import { listFolders } from "./listFolders";
 import { listSharedDrives } from "./listSharedDrives";
 import { move } from "./move";
+
+declare const global: google.script.PublicEndpoints;
 
 global.doGet = doGet;
 global.listFolders = listFolders;
