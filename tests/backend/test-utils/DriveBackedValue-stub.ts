2a8fc65781bc8d7ac3d7be21cb686e11ac81e0af
Marek Dědič <developer@dedic.eu>
2025-02-02 23:02:49 +0100

    Added types to vi.fn()

--- tests/backend/test-utils/DriveBackedValue-stub.ts
+++ tests/backend/test-utils/DriveBackedValue-stub.ts
@@ -4,6 +4,11 @@ import type { DriveBackedValue_ } from "../../../src/backend/utils/DriveBackedVa
 import type { MoveContext } from "../../../src/interfaces/MoveContext";
 import type { MoveError } from "../../../src/interfaces/MoveError";
 
+interface T {
+  errors: Array<MoveError>;
+  pathsToProcess: Array<MoveContext>;
+}
+
 export function mockedDriveBackedValue(): MockedObject<
   DriveBackedValue_<{
     errors: Array<MoveError>;
@@ -12,13 +17,8 @@ export function mockedDriveBackedValue(): MockedObject<
 > {
   // eslint-disable-next-line vitest/prefer-vi-mocked -- Acceptable as return value
   return {
-    deleteValue: vi.fn(),
-    loadValue: vi.fn(),
-    saveValue: vi.fn(),
-  } as unknown as MockedObject<
-    DriveBackedValue_<{
-      errors: Array<MoveError>;
-      pathsToProcess: Array<MoveContext>;
-    }>
-  >;
+    deleteValue: vi.fn<() => void>(),
+    loadValue: vi.fn<() => T | null>(),
+    saveValue: vi.fn<(value: T) => void>(),
+  } as unknown as MockedObject<DriveBackedValue_<T>>;
 }
